5af9fcef3bb7184d83b5dfe35efd18ca348617f8
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 <Vivekpatil254@gmail.com>
2022-04-04 15:19:19 +0530

    Last Commit

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,6 +4,28 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number between 0 to 9");
+        int number =scanner.nextInt();
+        if(number==0)
+            System.out.println("Zero");
+        else if(number ==1)
+            System.out.println("One");
+        else if(number==2)
+            System.out.println("Two");
+        else if(number ==3)
+            System.out.println("Three");
+        else if(number==4)
+            System.out.println("Four");
+        else if(number ==5)
+            System.out.println("Five");
+        else if(number==6)
+            System.out.println("Six");
+        else if(number ==7)
+            System.out.println("Seven");
+        else if(number==8)
+            System.out.println("Eight");
+        else if(number ==9)
+            System.out.println("Nine");
     }
 }
